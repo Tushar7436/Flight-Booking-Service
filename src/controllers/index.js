313fab071c899fef07dcb50c3d630840a1c683c0
Tushar Agarwal <tushar7436@gmail.com>
2025-08-31 14:59:50 +0530

    Basic setup

---
+++ src/controllers/index.js
@@ -0,0 +1,5 @@
+module.exports={
+    AboutControllers: require('./about-controller.js'),
+    HomeControllers: require('./home-controller.js'),
+    InfoController: require('./info-controller.js'),
+}
